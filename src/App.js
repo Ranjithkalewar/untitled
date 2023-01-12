ccbdab7e03e2894f524f5b6467b666ae056918fb
kalewarranjith <Kalewars99>
2023-01-12 23:35:12 +0100

    fundamentals in React

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react"
 import './App.css'
 import Img from "./Img";
 const App = () => {
-    const arr=[
-        "http://docs.google.com/uc?export=open&id=19-Q9JSH3ZvC40lOlUttxf4wtPVm9ctIU",
+    const arr=[1,2,3
+        /*"http://docs.google.com/uc?export=open&id=19-Q9JSH3ZvC40lOlUttxf4wtPVm9ctIU",
         "http://docs.google.com/uc?export=open&id=1QB2q13sop1x86CbOf5c5NBY0geWhk2hZ",
-        "http://docs.google.com/uc?export=open&id=1ARbwqW_uOLb4eCeWsyCJVZSYUIreXsrD",
+        "http://docs.google.com/uc?export=open&id=1ARbwqW_uOLb4eCeWsyCJVZSYUIreXsrD",*/
 
     ]
     return (
