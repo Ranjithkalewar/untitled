0ec90d21536d6415212286d00aeb539d9df5dc0a
kalewarranjith <Kalewars99>
2023-01-19 08:51:00 +0100

    fundamentals details in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
+import Test from "./Test";
 const App = () => {
-    let name="Ranjith"
+    const [data,useData] = useState({
+        name:"Ranjith",
+        age:26,
+        study:"Engineering",
+        email:"ranjithkumarkalewar@yahoo.com",
+        phone:96803087,
+    })
+    const name=data.name,age=data.age,study=data.study,email=data.email,phone=data.phone;
     return (
         <div>
             <center>
-                <h3>Hello {name}</h3>
+                <h2>
+                    <Test name={name} age={age} study={study} email={email} phone={phone}/>
+                </h2>
             </center>
         </div>
     )
--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,11 @@
 import React from "react"
 
 const Test = (props) => {
-        return (
+        return(
             <div>
                 <center>
                     <h2>
-                      My name is {props.name} and im {props.age} years old
+                        hello my name is {props.name} and my age is {props.age}. I have completed my {props.study} form jawaharlal Engineering college. My email address is {props.email} and my phone number is {props.phone}.
                     </h2>
                 </center>
             </div>
