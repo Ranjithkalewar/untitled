ae20eb9f31641d85af5abb3384fb2005dd28262b
kalewarranjith <Kalewars99>
2023-01-20 08:53:31 +0100

    Adding Classes in ReactJS

--- src/Test.js
+++ src/Test.js
@@ -1,14 +1,18 @@
 import React from "react"
-
-const Test = (props) => {
-        return(
+class Test extends React.Component {
+    render() {
+        /*const Test = (props) => {*/
+        return (
             <div>
                 <center>
                     <h2>
-                        Hello my name is {props.name} and my age is {props.age}. I have completed my {props.study} from Jawaharlal Nehru Engineering college. My email address is {props.email} and my phone number is +47 {props.phone}.
+                        Hello my name is {this.props.name} and my age is {this.props.age}. I have completed my {this.props.study} from
+                        Jawaharlal Nehru Engineering college. My email address is {this.props.email} and my phone number is
+                        +47 {this.props.phone}.
                     </h2>
                 </center>
             </div>
         )
+    }
 }
 export default Test;
